b6340f16d5484755a1a17af66e0fe0a9af62bbe9
lexlopez-dev <a.lopezcr9@gmail.com>
2020-02-16 19:31:08 -0700

    updates to fix aws

--- src/components/HomeHeader/HomeHeader.js
+++ src/components/HomeHeader/HomeHeader.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Swiper from 'swiper';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Swiper from "swiper/dist/js/swiper.js";
 
-import './HomeHeader.scss';
+import "./HomeHeader.scss";
 
 class HomeHeader extends Component {
-
   // Compare the item genres with the genre list and returns genre names
   handleGetGenre = genreId => {
     let mainGenre;
@@ -18,54 +17,74 @@ class HomeHeader extends Component {
     }
 
     return mainGenre;
-  }
+  };
 
   render() {
     const config = this.props.MDBConfig;
 
     // Initiates carousels
     (() => {
-      const sliderEl = document.querySelector('.home-swiper-container');
-       if(!sliderEl){
-         return;
-       }
-       const slider = new Swiper(sliderEl, {
-         slidesPerView: 1,
-         loop: true,
-         spaceBetween: 0,
-         observer: true,
+      const sliderEl = document.querySelector(".home-swiper-container");
+      if (!sliderEl) {
+        return;
+      }
+      const slider = new Swiper(sliderEl, {
+        slidesPerView: 1,
+        loop: true,
+        spaceBetween: 0,
+        observer: true,
 
-         autoplay: {
-          delay: 10000,
+        autoplay: {
+          delay: 10000
         },
 
-         pagination: {
-           el: '.home-swiper-pagination',
-           type: 'progressbar',
-         },
-         navigation: {
-           nextEl: '.home-swiper-button-next',
-           prevEl: '.home-swiper-button-prev',
-         }
-       });
+        pagination: {
+          el: ".home-swiper-pagination",
+          type: "progressbar"
+        },
+        navigation: {
+          nextEl: ".home-swiper-button-next",
+          prevEl: ".home-swiper-button-prev"
+        }
+      });
     })();
 
-    return(
+    return (
       <div className="home-swiper-container">
-
         {/* Loops through items and creates a carousel item */}
         <div className="swiper-wrapper">
           {this.props.items.map((item, i) => {
             if (i > 3 && i < 7) {
               return (
-                <Link key={item.id} to={`/details/${this.props.itemType.toLowerCase()}/${item.id}`} className="swiper-slide" style={{background: `linear-gradient(
+                <Link
+                  key={item.id}
+                  to={`/details/${this.props.itemType.toLowerCase()}/${
+                    item.id
+                  }`}
+                  className="swiper-slide"
+                  style={{
+                    background: `linear-gradient(
                 rgba(0, 0, 0, 0.6),
                 rgba(0, 0, 0, 0.6)
-              ) center center no-repeat, #fff url(${config.images ? config.images.secure_base_url : ''}${config.images ? config.images.backdrop_sizes[2] : ''}${item.backdrop_path}) center top no-repeat`, backgroundSize: 'cover, cover'}}>
+              ) center center no-repeat, #fff url(${
+                config.images ? config.images.secure_base_url : ""
+              }${config.images ? config.images.backdrop_sizes[2] : ""}${
+                      item.backdrop_path
+                    }) center top no-repeat`,
+                    backgroundSize: "cover, cover"
+                  }}
+                >
                   <div>
-                    <p className="swiper-slide__category">{'Latest'.toUpperCase()}</p>
-                    <h2 className="swiper-slide__title">{this.props.itemType === 'MOVIE'? item.title : item.name}</h2>
-                    <p className="swiper-slide__item-duration">{this.handleGetGenre(item.genre_ids)} | {item.vote_average} Rating</p>
+                    <p className="swiper-slide__category">
+                      {"Latest".toUpperCase()}
+                    </p>
+                    <h2 className="swiper-slide__title">
+                      {this.props.itemType === "MOVIE" ? item.title : item.name}
+                    </h2>
+                    <p className="swiper-slide__item-duration">
+                      {this.handleGetGenre(item.genre_ids)} |{" "}
+                      {item.vote_average} Rating
+                    </p>
                   </div>
                 </Link>
               );
@@ -74,10 +93,9 @@ class HomeHeader extends Component {
         </div>
 
         <div className="home-swiper-pagination"></div>
-
       </div>
     );
   }
-};
+}
 
 export default HomeHeader;
--- src/components/TrailerCarousel/TrailerCarousel.js
+++ src/components/TrailerCarousel/TrailerCarousel.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react';
-import Swiper from 'swiper'
-import './TrailerCarousel.scss'
+import React, { Component } from "react";
+import Swiper from "swiper/dist/js/swiper.js";
+import "./TrailerCarousel.scss";
 
 class TrailerCarousel extends Component {
-
   render() {
-
     // Initiates carousel
     (() => {
-      const sliderEl = document.querySelectorAll('.trailer-swiper-container');
-       if(!sliderEl){
-         return;
-       }
-       const slider = new Swiper(sliderEl, {
-         init: true,
-         slidesPerView: 3,
-         loop: true,
-         spaceBetween: 0,
-         observer: true,
-         centeredSlides: true,
+      const sliderEl = document.querySelectorAll(".trailer-swiper-container");
+      if (!sliderEl) {
+        return;
+      }
+      const slider = new Swiper(sliderEl, {
+        init: true,
+        slidesPerView: 3,
+        loop: true,
+        spaceBetween: 0,
+        observer: true,
+        centeredSlides: true,
 
-         breakpoints: {
-           1500: {
-             slidesPerView: 2,
-           },
-           800: {
-             slidesPerView: 1,
-           }
-         },
-         pagination: {
-           el: '.swiper-pagination',
-           clickable: true
-         },
-         navigation: {
-           nextEl: '.swiper-button-next',
-           prevEl: '.swiper-button-prev',
-         }
-       });
+        breakpoints: {
+          1500: {
+            slidesPerView: 2
+          },
+          800: {
+            slidesPerView: 1
+          }
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      });
     })();
 
-    return(
+    return (
       <div className="trailer-carousel">
         <div className="trailer-swiper-container">
-
           <div className="swiper-wrapper">
             {this.props.movieTrailers.results.map(trailer => (
-
               <div key={trailer.key} className="swiper-slide">
-                <iframe title="1" width="420" height="315"src={`https://www.youtube.com/embed/${trailer.key}`}></iframe>
+                <iframe
+                  title="1"
+                  width="420"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${trailer.key}`}
+                ></iframe>
               </div>
-
             ))}
           </div>
 
